bbd1b2f00af7921ff1ffb7dc3aeeccaa02e3ebdb
luizlenharo <lflenharo@gmail.com>
2025-09-09 17:03:37 -0300

    feat: implementa endpoint para listar gastos do usuário

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,26 @@
 from fastapi import FastAPI, HTTPException, status
-from .models.userCreate import UserCreate
-from .models.user import User
-from .models.expenseCreate import ExpenseCreate
+
 from .models.expense import Expense
+from .models.expenseCreate import ExpenseCreate
+from .models.user import User
+from .models.userCreate import UserCreate
 from .utils.password_hash import get_password_hash
-from .utils.validators import validate_email_format, validate_password_strength, validate_date_ISO_format
+from .utils.validators import (
+    validate_date_ISO_format,
+    validate_email_format,
+    validate_password_strength,
+)
 
 app = FastAPI()
 
 # "Banco de Dados" em memória (apenas para testes)
-dict_db = {}
+users_db = {}
 user_id = 0
+
 
 @app.post("/users", response_model=User, status_code=status.HTTP_201_CREATED)
 def create_user(user_data: UserCreate):
-    '''
+    """
     Criação de um novo usuário no sistema
 
     Parâmetros:
@@ -27,7 +33,7 @@ def create_user(user_data: UserCreate):
     HTTPException:
         - 400 BAD REQUEST se o email já estiver cadastrado
         - 400 BAD REQUEST se o formato do email não for válido ou a senha não atender os critérios de segurança
-    '''
+    """
     global user_id
 
     # Valida o formato do email
@@ -37,35 +43,37 @@ def create_user(user_data: UserCreate):
     validate_password_strength(user_data.password)
 
     # Verifica se o email já existe no "banco"
-    for user in dict_db.values():
+    for user in users_db.values():
         if user["email"] == user_data.email:
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
-                detail="Este email já está cadastrado."
+                detail="Este email já está cadastrado.",
             )
-    
+
     # Gera o hash da senha para não armazenar a senha original
     hashed_password = get_password_hash(user_data.password)
     new_user = {
         "id": user_id,
         "email": user_data.email,
-        "hashed_password": hashed_password 
+        "hashed_password": hashed_password,
     }
     user_id += 1
 
     # Armazena o novo usuário no dicionário
-    dict_db[new_user["id"]] = new_user
+    users_db[new_user["id"]] = new_user
 
     # Retorna os dados do usuário, seguindo o modelo User definido em models
     return new_user
+
 
 # "Banco de Dados" de um usuário X em memória (apenas para testes)
 expenses_db = {}
 expenses_id = 0
 
+
 @app.post("/expenses", response_model=Expense, status_code=status.HTTP_201_CREATED)
 def create_expense(user: User, expense: ExpenseCreate):
-    '''
+    """
     Criação de um novo gasto no sistema
 
     Parâmetros:
@@ -84,41 +92,40 @@ def create_expense(user: User, expense: ExpenseCreate):
         - 400 BAD REQUEST se o valor informado for menor ou igual a zero
         - 400 BAD REQUEST se o formato da data informada não estiver em ISO String
         - 403 FORBIDDEN se o usuário não estiver cadastrado
-    '''
+    """
     global expenses_id
 
-    if (user.id in dict_db):
-
-        stored_user = dict_db[user.id]
+    if user.id in users_db:
+        stored_user = users_db[user.id]
 
         if stored_user["email"] != user.email:
             raise HTTPException(
                 status_code=status.HTTP_403_FORBIDDEN,
-                detail="As credenciais do usuário (ID e Email) não correspondem."
-            )
- 
-        valid_categories = ["Alimentação", "Lazer", "Contas"]
-        if (expense.category not in valid_categories):
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail="Categoria informada é inválida. Informe uma entre [Alimentação; Lazer; Contas]."
+                detail="As credenciais do usuário (ID e Email) não correspondem.",
             )
 
-        if (expense.value <= 0):
+        valid_categories = ["Alimentação", "Lazer", "Contas"]
+        if expense.category not in valid_categories:
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
-                detail="Valor informado é inválido. Informe um valor maior ou igual a zero."
+                detail="Categoria informada é inválida. Informe uma entre [Alimentação; Lazer; Contas].",
+            )
+
+        if expense.value <= 0:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Valor informado é inválido. Informe um valor maior ou igual a zero.",
             )
 
         # Valida se a data está em formato ISO String
         validate_date_ISO_format(expense.date)
-        
+
         new_expense = {
             "id": expenses_id,
             "user_id": user.id,
             "category": expense.category,
             "date": expense.date,
-            "value": expense.value
+            "value": expense.value,
         }
         expenses_id += 1
 
@@ -128,5 +135,39 @@ def create_expense(user: User, expense: ExpenseCreate):
     else:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
-            detail="Usuário não cadastrado. Cadastre-se antes de registrar um gasto."
+            detail="Usuário não cadastrado. Cadastre-se antes de registrar um gasto.",
         )
+
+
+@app.get("/users/{user_id}/expenses", response_model=list[Expense])
+def get_expenses_by_user(user_id: int):
+    """
+    Lista todos os gastos de um usuário específico
+
+    Parâmetros:
+    user_id (int): inteiro representando o id do usuário que queremos consultar os gastos
+
+    Retorna:
+    list[Expense]: uma list contendo todos os gatos do usuário
+    Obs: retorna uma lista vazia caso o usuário não tenha nenhum gasto registrado
+
+    Levanta:
+    HTTPException:
+        - 404 NOT FOUND se o usuário com o ID fornecido não foe encontrado
+    """
+
+    # Verifica se o usuário existe
+    if user_id not in users_db:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Usuário com ID {user_id} não encontrado.",
+        )
+
+    user_expenses = []
+    for expense in (
+        expenses_db.values()
+    ):  # Para cada gasto cadastrado adiciona apenas do usuário desejado
+        if expense["user_id"] == user_id:
+            user_expenses.append(expense)
+
+    return user_expenses
