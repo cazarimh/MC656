c9924b53df1b6bd854b23fb8dfe2bf0726245d8b
Mateus Farha Ribeiro <mateusfarha0@gmail.com>
2025-11-23 14:38:56 -0300

    Integrando backend com a tela de login

---
+++ frontend/src/api/userApi.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8000/users";
+
+export async function registerUser(userData) {
+  try {
+    const response = await axios.post(`${API_URL}/`, userData);
+    return response.data;
+  } catch (error) {
+    console.error("----- ERRO COMPLETO DO BACKEND -----");
+    console.log("response:", error.response);
+    console.log("data:", error.response?.data);
+
+    if (error.response && error.response.data && error.response.data.detail) {
+      throw new Error(error.response.data.detail);
+    }
+
+    throw new Error("Erro ao registrar usuário");
+  }
+}
+
+export async function loginUser(userData) {
+  try {
+    const response = await axios.post(`${API_URL}/login`, userData);
+    return response.data;
+  } catch (error) {
+    console.error("Erro no login:", error);
+
+    if (error.response && error.response.data && error.response.data.detail) {
+      throw new Error(error.response.data.detail);
+    }
+
+    throw new Error("Credenciais inválidas");
+  }
+}
